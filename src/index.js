cd0983b18a25315df96e6b643e5b056178be4441
Matrui <yanmatr@gmail.com>
2023-08-09 13:19:38 +0200

    init: towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,49 @@
 
 // You should implement your task here.
+// const matrix1 = [
+//   [ 1, 2, 3 ],
+//   [ 4, 5, 6 ],
+//   [ 7, 8, 9 ],
+//  ];
+//  let ourArr = []
+ 
+//  var resultArr = matrix1.reduce(function(first, current, index) {
+//   if( index % 2 === 0){
+//     for (let matrixLine of current) {
+//       ourArr.push(matrixLine);
+//   };
+//   }else{
+//     current.reverse();
+//     for (let matrixLine of current) {
+//       ourArr.push(matrixLine);
+//   };
+//   };
+//   return ourArr;
+//  }, 0);
+
+
 
 module.exports = function towelSort (matrix) {
+  
+ if (matrix == undefined){
   return [];
-}
+ }
+ let ourArr = []
+ 
+ var resultArr = matrix.reduce(function(first, current, index) {
+  if( index % 2 === 0){
+    for (let matrixLine of current) {
+      ourArr.push(matrixLine);
+  };
+  }else{
+    current.reverse();
+    for (let matrixLine of current) {
+      ourArr.push(matrixLine);
+  };
+  };
+  return ourArr;
+ }, 0);
+  
+ return ourArr;
+};
+
